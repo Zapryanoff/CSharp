a5a386b403d06398d2c86558ae657659fb793d94
Димитър Запрянов <126179351+Zapryanoff@users.noreply.github.com>
2024-10-15 20:20:36 +0300

    Add files via upload

---
+++ minutesAndSeconds.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());
+
+            int sum = first + second + third;
+            int minutes = 0;
+            int seconds = 0;
+
+                minutes = sum / 60;
+                seconds = sum % 60;
+            
+            if (seconds < 10)
+            {
+                Console.WriteLine(minutes + ":0" + seconds);
+            }
+            else
+            {
+                Console.WriteLine(minutes + ":" + seconds);
+            }
+        }
+    }
+}
