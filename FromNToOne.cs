a8a5f433bde2301c8f5a40837b31293a07725ca0
Димитър Запрянов <detailedyt@gmail.com>
2024-10-18 09:57:29 +0300

    Add files via upload

---
+++ FromNToOne.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            
+            for (int i = n; i >= 1; i--) { 
+            
+                System.Console.WriteLine(i);
+            }
+        }
+    }
+}
